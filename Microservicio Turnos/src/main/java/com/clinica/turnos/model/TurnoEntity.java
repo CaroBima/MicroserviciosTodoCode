1cfee3beaac889903a147d29c5b78891bccc4ee0
CaroBima <carobima@gmail.com>
2023-10-31 15:49:29 -0300

    Definicion atributos en clases Paciente y Turno, configuracion de la bbdd en application.properties

--- Microservicio Turnos/src/main/java/com/clinica/turnos/model/TurnoEntity.java
+++ Microservicio Turnos/src/main/java/com/clinica/turnos/model/TurnoEntity.java
@@ -1,10 +1,25 @@
 package com.clinica.turnos.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class TurnoEntity {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Temporal(TemporalType.DATE)
+    private LocalDate fecha;
+    private String tratamiento;
+    private String nombreCompletoPaciente;
 }
